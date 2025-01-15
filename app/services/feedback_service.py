68389f6d813206c473a3096d0ee8ebadc50530fe
김홍석 <drghdtjr@naver.com>
2025-01-15 17:53:33 +0900

    fix:캐릭터 테이블 버블 테이블 삭제 및 반환 변경

--- app/services/feedback_service.py
+++ app/services/feedback_service.py
@@ -1,18 +1,18 @@
-from sqlalchemy.orm import Session, joinedload
-from app.models.feedback import Feedback
-from app.schemas.user import UserWithFeedback
-from app.models.user import User
-
-def get_feedbacks(user: User, db: Session):
-    feedbacks = db.query(Feedback).options(
-        joinedload(Feedback.sentence)
-    ).filter(
-        Feedback.user_id == user.user_id,
-    ).all()
-    user_with_feedback = UserWithFeedback(
-        user_id=user.user_id,
-        email=user.email,
-        nickname=user.nickname,
-        feedbacks=feedbacks
-    )
-    return user_with_feedback
+# from sqlalchemy.orm import Session, joinedload
+# from app.models.feedback import Feedback
+# from app.schemas.user import UserWithFeedback
+# from app.models.user import User
+#
+# def get_feedbacks(user: User, db: Session):
+#     feedbacks = db.query(Feedback).options(
+#         joinedload(Feedback.sentence)
+#     ).filter(
+#         Feedback.user_id == user.user_id,
+#     ).all()
+#     user_with_feedback = UserWithFeedback(
+#         user_id=user.user_id,
+#         email=user.email,
+#         nickname=user.nickname,
+#         feedbacks=feedbacks
+#     )
+#     return user_with_feedback
